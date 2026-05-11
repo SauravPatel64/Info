d7719e8763b2dcbad3b8cb9a3f053cc1cd6357d8
e <sauravpatel220930@gmail.com>
2026-05-11 07:38:28 +0530

    d

--- May/Day8/ass1.py
+++ May/Day8/ass1.py
@@ -1,11 +1,12 @@
 """
 1)WAP to find out the sum of all integer between 100 and 200 which are divisible by 9
 """
+
 sum = 0
-i = 100
+i=100
 while i<200:
     if i%9==0:
-        sum = sum + i
+        sum = sum+i
     i+=1
-    
+
 print(sum)
