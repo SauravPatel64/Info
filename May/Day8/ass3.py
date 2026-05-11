ba1ea1a127a9e2dfb026c6e4170e2a8fed2a64a1
e <sauravpatel220930@gmail.com>
2026-05-11 11:10:23 +0530

    ass3

--- May/Day8/ass3.py
+++ May/Day8/ass3.py
@@ -1,4 +1,12 @@
-    """
+"""
     3)	WAP to find out all the leap years between two entered years
-    """
+"""
+
+first = int(input("Enter Starting Year =  "))
+second = int(input("Enter Second Year = "))
+
+for i in range(first, second):
+    if (i%4==0 and i%100!=0) or (i%400==0):
+        print(i)
+        
     
